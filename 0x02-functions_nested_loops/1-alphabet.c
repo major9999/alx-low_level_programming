f824828843c1dda7649b44b720a5ef427cacbe36
Mohammed Faris <momohamed2157@gmail.com>
2023-03-21 23:30:54 +0000

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet on console with a new line
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
