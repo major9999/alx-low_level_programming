c7b4016e7eb51fc2b4504774bd6b8280de5f7751
Mohammed Faris <momohamed2157@gmail.com>
2023-03-22 02:41:07 +0000

    Natural 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - list all the natural numbers.
+ * Return: 0 and exit the program.
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
