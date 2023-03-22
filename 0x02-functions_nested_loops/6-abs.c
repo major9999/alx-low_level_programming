4b5a295e1d8c5cc985719eccf78fef2ac5f4b130
Mohammed Faris <momohamed2157@gmail.com>
2023-03-22 00:01:12 +0000

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute integer value of a number.
+ * @n: the number for the test.
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
