87f18d36f6857d38f5b44fbc4c62b447bd9f626a
Mohammed Faris <momohamed2157@gmail.com>
2023-04-09 09:14:31 +0000

    num of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main program entry point.
+ * @argc: the number of args.
+ * @argv: the array of args.
+ *
+ * Return: just a 0.
+ */
+
+int main(int argc, char __attribute__((unused))*argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
