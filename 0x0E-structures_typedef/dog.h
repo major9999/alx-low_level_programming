0838519b18a49713dfd5260792028e1250a2bf52
major999 <momohamed2157@gmail.com>
2023-04-18 02:20:49 +0000

    my dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef MYFDOG_H
+#define MYFDOG_H
+
+/**
+ * struct dog - The Dog main struct.
+ * @name: The dog name goes here.
+ * @age: The age of that dog.
+ * @owner: Who the owner? of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
