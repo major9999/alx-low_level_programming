b42a2dc631178e5c35d1b920453663ad7aa15099
major999 <momohamed2157@gmail.com>
2023-05-03 01:44:13 +0000

    asdasd

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - qwoidhwqiudiwqudwqd.
+ * @h: uykuyikyukuk.
+ * Return: poiqwjdio9wqd.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t mypwoqs = 0;
+
+	while (h != NULL)
+
+	{
+		h = h->next;
+		mypwoqs++;
+	}
+
+	return (mypwoqs);
+}
