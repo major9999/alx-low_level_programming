92ffb26d02d9a6c0e77c429531271e853c9fe577
major999 <momohamed2157@gmail.com>
2023-05-04 22:53:38 +0000

    the 100 challenge

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - oijdwqiojdwqoijwqdqwidjq.
+ * Return: oidwhqueghfuihnwqiudhjqw.
+ */
+
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if (*endian == 1)
+
+	{
+		return (1);
+	}
+
+	return (0);
+}
