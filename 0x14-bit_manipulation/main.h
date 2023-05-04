022b1e2e8fdbbc911f19c24ff831dff1c5f1e10c
major999 <momohamed2157@gmail.com>
2023-05-04 22:41:07 +0000

    my main

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef MYWIOJWID_H
+#define MYWIOJWID_H
+
+int _putchar(char c);
+unsigned long int _power(unsigned int base, unsigned int pow);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+#endif
